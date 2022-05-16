ae5ca476a07c67a2a7698f289f61824a5f7b465c
Giovanni Foletto <giovanni.foletto@gmail.com>
2022-05-16 16:20:53 +0200

    Added config.js to Backend.

--- backend/server.js
+++ backend/server.js
@@ -1,12 +1,13 @@
 
 const express = require('express')
+const config = require('./config')
 const app = express()
-const port = 3000
+
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
 })
 
-app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
+app.listen(config.PORT, () => {
+  console.log(`BarAdvisor-API listening on ${config.PORT}`)
 })
