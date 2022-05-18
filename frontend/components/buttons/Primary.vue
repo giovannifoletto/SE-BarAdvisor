007209b6d4bf1a890464bf36cea7cbe8f52074bd
Giovanni Foletto <giovanni.foletto@gmail.com>
2022-05-18 13:40:36 +0200

    Frontend init e completata interfaccia di base.

---
+++ frontend/components/buttons/Primary.vue
@@ -0,0 +1,32 @@
+<script>
+export default {
+  data() {
+    return {
+      title: "Titolo Bottone",
+
+    }
+  },
+  props: ["title"]
+}
+</script>
+
+<template>
+  <button type="button" class="primary-button">{{ title }}</button>
+</template>
+
+<style scoped>
+.primary-button {
+  border-bottom: 1px solid black;
+  background-color: #ff9922;
+  color: black;
+  border: none;
+  padding: 5px;
+  font: inherit;
+  cursor: pointer;
+  outline: inherit;
+  border-radius: 5px;
+}
+.primary-button:hover {
+  background-color: var(--primaryHover);
+}
+</style>
---
+++ frontend/components/buttons/Secondary.vue
@@ -0,0 +1,28 @@
+<script>
+export default{
+  props: ['title'] 
+}
+</script>
+
+<template>
+  <button class="secondary-button" type="button">{{ title }}</button>
+</template>
+
+
+<style scoped>
+.secondary-button {
+  background-color: none;
+  color: black;
+  border: none;
+  font: inherit;
+  cursor: pointer;
+  outline: inherit;
+  padding: 5px;
+  border-radius: 5px;
+  margin-left: auto;
+  margin-right: auto;
+}
+.secondary-button:hover {
+  background-color: #a6a6a6;
+}
+</style>
