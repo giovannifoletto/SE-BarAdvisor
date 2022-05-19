d4db87a276cc4869e644c01dbecb1c87d38666e7
Giovanni Foletto <giovanni.foletto@gmail.com>
2022-05-20 01:27:15 +0200

    Aggiunta components.

---
+++ frontend/components/EventPost.vue
@@ -0,0 +1,56 @@
+<script>
+export default {
+    props: ["nome", "dataInizio", "descrizione", "route"]
+}
+</script>
+
+<template>
+  <div class="post">
+    <div class="title pt-2">
+      <div class="account">
+        <div>
+          <IconsAccount />
+        </div>
+        <!-- <NuxtLink to="route" class="px-3">-->
+          <h3>{{ nome }}</h3>
+        <!-- </NuxtLink> -->
+      </div>
+    </div>
+    <div class="under">
+      <div class="info p-1">
+        <h6>Data di inizio</h6>
+        <h5>{{ dataInizio }}</h5>
+        <h6>informazioni</h6>
+        <h5>{{ descrizione }}</h5>
+      </div>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+	.post {
+		display: flex;
+		flex-flow: column nowrap;
+		border: 1px solid black;
+		border-radius: 4px;
+		justify-content: baseline;
+	}
+	.title {
+		display: flex;
+		flex-flow: row nowrap;
+		justify-content: space-between;
+		padding: 5px;
+		border-bottom: 1px solid black;
+	}
+	.account {
+		display: flex;
+		flex-flow: row nowrap;
+		justify-content: baseline;
+		padding-left: 5px;
+		display: flex;
+		column-gap: 10px;
+	}
+	.under {
+		text-align: center;
+	}
+</style>
--- frontend/pages/index.vue
+++ frontend/pages/index.vue
@@ -1,9 +1,26 @@
 <template>
-  <h1>Home Page</h1>
+  <main>
+    <h1>Home Page</h1>
+
+    <EventPost
+      dataInizio="Data di inizio"
+      descrizione="Descrizione dell'evento"
+      nome="Nome dell'evento"
+      
+    />
+  </main>
 </template>
 
 <script>
+import EventPost from "../components/EventPost.vue";
 export default {
-  name: 'IndexPage'
-}
+  name: "IndexPage",
+  components: { EventPost },
+};
 </script>
+
+<style scoped>
+main {
+  text-align: center;
+}
+</style>
