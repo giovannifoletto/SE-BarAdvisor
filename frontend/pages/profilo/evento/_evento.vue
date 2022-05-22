6129a236537f538806d4c6c027be9b3db549394b
Giovanni Foletto <giovanni.foletto@gmail.com>
2022-05-22 23:21:53 +0200

    Aggiunti profilo evento e locale.

--- frontend/pages/profilo/evento/_evento.vue
+++ frontend/pages/profilo/evento/_evento.vue
@@ -1,6 +1,19 @@
 <template>
-  <div>
-    {{ pageUrl }}
+  <div class="event">
+    <div class="title">
+      <div>
+        <h3>{{ evento.nome }}</h3>
+      </div>
+    </div>
+    <div class="location py-3 text-center">
+      <NuxtLink :to="evento.locale">
+        <ButtonsSecondary title="Visita il Locale gestore dell'evento" />
+      </NuxtLink>
+    </div>
+
+    <div class="info py-3 text-center">
+      <h5>{{ evento.informazioni }}</h5>
+    </div>
   </div>
 </template>
 
@@ -8,9 +21,30 @@
 export default {
   data() {
     return {
-      pageUrl: this.$route.params,
+      evento: null,
     };
   },
-  middleware: 'auth',
+  middleware: "auth",
+  async fetch() {
+    // this.evento = this.$axios.get(`/eventi/${this.$route.params}`)
+    this.evento = {
+      nome: "Nome evento",
+      locale: "idLocale",
+      informazioni: "informazione",
+      descrizione: "Descrizione",
+    };
+  },
 };
 </script>
+
+
+<style>
+.title {
+  display: flex;
+  flex-flow: row nowrap;
+  justify-content: space-between;
+  justify-items: baseline;
+  padding: 10px;
+  border-bottom: 1px solid black;
+}
+</style>
--- frontend/pages/profilo/locale/_locale.vue
+++ frontend/pages/profilo/locale/_locale.vue
@@ -1,6 +1,14 @@
 <template>
-  <div>
-    {{ pageUrl }}
+  <div class="event">
+    <div class="title">
+      <div>
+        <h3>{{ locale.nome }}</h3>
+      </div>
+    </div>
+    <div class="info py-3 text-center">
+      <h5>{{ locale.informazioni }}</h5>
+      <h5>{{ locale.descrizione }}</h5>
+    </div>
   </div>
 </template>
 
@@ -8,9 +16,30 @@
 export default {
   data() {
     return {
-      pageUrl: this.$route.params,
+      locale: null,
     };
   },
-  middleware: 'auth',
+  middleware: "auth",
+  async fetch() {
+    // this.locale = this.$axios.get(`/eventi/${this.$route.params}`)
+    this.locale = {
+      nome: "Nome locale",
+      locale: "idLocale",
+      informazioni: "informazione",
+      descrizione: "Descrizione",
+    };
+  },
 };
 </script>
+
+
+<style>
+.title {
+  display: flex;
+  flex-flow: row nowrap;
+  justify-content: space-between;
+  justify-items: baseline;
+  padding: 10px;
+  border-bottom: 1px solid black;
+}
+</style>
