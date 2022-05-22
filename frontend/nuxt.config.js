1a40ac71773b5f4d578e7c01def2af2044c97f75
Giovanni Foletto <giovanni.foletto@gmail.com>
2022-05-22 18:59:13 +0200

    Docker compose.

--- frontend/nuxt.config.js
+++ frontend/nuxt.config.js
@@ -53,10 +53,14 @@ export default {
     '@nuxtjs/axios',
   ],
 
+  env: {
+    baseUrl: process.env.BASE_URL || '/'
+  },
+
   // Axios module configuration: https://go.nuxtjs.dev/config-axios
   axios: {
     // Workaround to avoid enforcing hard-coded localhost:3000: https://github.com/nuxt-community/axios-module/issues/308
-    baseURL: '/api/v1/',
+    baseURL: `${process.env.baseUrl}/api/v1/`,
   },
 
   // Build Configuration: https://go.nuxtjs.dev/config-build
