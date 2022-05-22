2dbc5f009cf29d89302fc16f2b9691b3975c0e8f
Giovanni Foletto <giovanni.foletto@gmail.com>
2022-05-22 22:54:38 +0200

    Missing endpoint.

--- frontend/components/layout/LeftDiv.vue
+++ frontend/components/layout/LeftDiv.vue
@@ -1,5 +1,20 @@
 <script>
 export default {
+  data() {
+    return {
+      passwordData: {
+        visible: false,
+        buttonText: "Cambia Password",
+      },
+      error: {
+        status: false,
+        errorText: null,
+      },
+      oldPassword: null,
+      newPassword1: null,
+      newPassword2: null,
+    };
+  },
   methods: {
     logOut() {
       try {
@@ -14,7 +29,28 @@ export default {
         console.log("No sessionStorage token");
       }
 
-      window.location.href = "/"
+      window.location.href = "/";
+    },
+    async togglePassData() {
+      if (this.passwordData.visible) {
+        this.passwordData.visible = false;
+        this.passwordData.buttonText = "Cambia Password";
+        // TODO: data di cambia password ?
+        try {
+          const changeP = this.$axios({
+            url: `TO-BE-DEFINED`,
+            method: "POST",
+            data: JSON.stringify(),
+          });
+        } catch (err) {
+          console.log(err);
+          this.error.status = true;
+          this.error.errorText = err || "Errore imprevisto, riprovare";
+        }
+      } else {
+        this.passwordData.buttonText = "Clicca per continuare";
+        this.passwordData.visible = true;
+      }
     },
   },
 };
@@ -23,14 +59,50 @@ export default {
 <template>
   <div>
     <h1>Account</h1>
-    <ButtonsPrimary
-      title="Logout"
-      @buttonClicked="
-        {
-          logOut();
-        }
-      "
-    />
+    <div class="myflex-col">
+      <ButtonsPrimary title="Logout" @buttonClicked="logOut()" />
+      <div id="cambiaPassword" v-if="passwordData.visible">
+        <ErrorDiv
+          :errorText="error.errorText"
+          v-if="error.status"
+          @dismissError="error.status = !error.status"
+        />
+        <div class="form-group">
+          <label for="inputPassword">Inserisci Password</label>
+          <input
+            v-bind="oldPassword"
+            type="password"
+            class="form-control"
+            placeholder="Inserisci Password"
+            required
+          />
+        </div>
+        <div class="form-group">
+          <label for="inputPassword">Nuova Password</label>
+          <input
+            v-bind="newPassword1"
+            type="password"
+            class="form-control"
+            placeholder="Inserisci Password"
+            required
+          />
+        </div>
+        <div class="form-group">
+          <label for="inputPassword">Ripeti nuova Password</label>
+          <input
+            v-bind="newPassword2"
+            type="password"
+            class="form-control"
+            placeholder="Inserisci Password"
+            required
+          />
+        </div>
+      </div>
+      <ButtonsPrimary
+        :title="passwordData.buttonText"
+        @buttonClicked="togglePassData()"
+      />
+    </div>
   </div>
 </template>
 
@@ -40,5 +112,11 @@ div {
 }
 h1 {
   padding-bottom: 25px;
+}
+.myflex-col {
+  display: flex;
+  flex-flow: column nowrap;
+  align-items: center;
+  gap: var(--default-padding);
 }
 </style>
