d22e137bc4e42e8b6dfeab9638a95253a7322343
diocarro <daniele.carraro@studenti.unitn.it>
2022-05-23 11:50:56 +0200

    creata funzione per prelevare informazioni di un Locale

--- backend/routes/locali.js
+++ backend/routes/locali.js
@@ -4,12 +4,16 @@ const router = express.Router()
 const checkRole = require('../middleware/check-role')
 const checkAuth = require('../middleware/check-auth')
 
-const eventiController = require('../controllers/controllerEvento')
+const controllerEventi = require('../controllers/controllerEvento')
+const controllerLocali = require('../controllers/controllerLocale')
+
+// recuperare un locale 
+router.get('/:localeID', controllerLocali.getLocale)
 
 // aggiungere un evento 
-router.post('/:localeID/eventi', checkAuth, checkRole.checkOwnerLocale, eventiController.postEvento)
+router.post('/:localeID/eventi', checkAuth, checkRole.checkOwnerLocale, controllerEventi.postEvento)
 
 // informazioni relative ad un evento specifico
-router.get('/:localeID/eventi/:eventoID', eventiController.getEvento)
+router.get('/:localeID/eventi/:eventoID', controllerEventi.getEvento)
 
 module.exports = router
