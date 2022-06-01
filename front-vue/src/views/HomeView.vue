c64c426c9fa68a01c33f870ab90f13882cd6bc3d
Giovanni Foletto <giovanni.foletto@gmail.com>
2022-06-01 18:35:16 +0200

    PostEvento.

--- front-vue/src/views/HomeView.vue
+++ front-vue/src/views/HomeView.vue
@@ -1,35 +1,41 @@
 <template>
   <div>
     <h1>bacheca</h1>
-    <div v-for="evento in eventi" :key="evento._id">
-      <div>
-        <h2>{{ evento?.nome }}</h2>
-        <h3>{{ evento?.locale?.nome }}</h3>
-      </div>
-    </div>
+    <PostEvento
+      v-for="evento in eventi"
+      :key="evento._id"
+      :nome="evento.nome"
+      :locale="evento.locale"
+      :descrizione="evento.descrizione"
+      :dataInizio="evento.dataInizio"
+    />
   </div>
 </template>
 
 <script>
+import PostEvento from "@/components/PostEvento";
 export default {
-  name: 'HomeView',
+  name: "HomeView",
+  components: {
+    PostEvento,
+    EventoView,
+  },
   data() {
     return {
-      eventi: []
-    }
+      eventi: [],
+    };
   },
   async mounted() {
     try {
-      const res = await fetch('http://localhost:4000/api/v1/eventi')
-      const data = await res.json()
+      const res = await fetch("http://localhost:4000/api/v1/eventi");
+      const data = await res.json();
 
       if (data.success) {
-        this.eventi = data.eventi
+        this.eventi = data.eventi;
       }
-
     } catch (error) {
-      console.log(error)
+      console.log(error);
     }
-  }
-}
+  },
+};
 </script>
