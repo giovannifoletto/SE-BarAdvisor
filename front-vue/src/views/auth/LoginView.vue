cdd90286de303822fef1a7e3fcbd954739684720
Giovanni Foletto <giovanni.foletto@gmail.com>
2022-06-01 14:26:10 +0200

    Aggiunte view.

---
+++ front-vue/src/views/auth/LoginView.vue
@@ -0,0 +1,78 @@
+<template>
+  <main>
+    <form>
+      <div class="form-group">
+        <label for="emailInput">Indirizzo Email</label>
+        <input
+          type="email"
+          class="form-control"
+          placeholder="Inserisci email"
+          required
+        />
+      </div>
+      <br />
+      <div class="form-group">
+        <label for="inputPassword">Password</label>
+        <input
+          type="password"
+          class="form-control"
+          placeholder="Inserisci Password"
+          required
+        />
+      </div>
+      <div class="form-group myflex">
+        <div class="form-check checkbox">
+          <input
+            class="form-check-input"
+            type="checkbox"
+            id="remainLoggedIn"
+          />
+          <div>
+            <label class="form-check-label" for="remainLoggedIn">
+              Resta collegato
+            </label>
+          </div>
+        </div>
+      </div>
+      <div class="myflex">
+
+        <NuxtLink to="/passwordrecovery">
+          <ButtonsSecondary title="Dimenticato la password" />
+        </NuxtLink>
+        <NuxtLink to="/signup">
+          <ButtonsSecondary title="Crea Nuovo Account" />
+        </NuxtLink>
+        <div>
+          <div class="py-1"></div>
+          <ButtonsPrimary title="Login" />
+        </div>
+      </div>
+    </form>
+  </main>
+</template>
+
+<style scoped>
+main {
+  min-height: 60vh;
+  padding: 15px;
+}
+form {
+  display: flex;
+  flex-flow: column nowrap;
+  justify-content: center;
+  padding: 10px;
+}
+.myflex {
+  display: flex;
+  flex-flow: column nowrap;
+  align-items: center;
+  gap: 5px;
+  padding-top: 10px;
+}
+.checkbox {
+  display: flex;
+  flex-flow: row nowrap;
+  align-items: center;
+  gap: 5px;
+}
+</style>
---
+++ front-vue/src/views/auth/RegistrazioneView.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="myflex">
+    <NuxtLink to="/signup/cliente">
+      <ButtonsSecondary title='Sono un cliente' />
+    </NuxtLink>
+    <NuxtLink to="/signup/gestorelocale">
+      <ButtonsSecondary title='Sono un gestore di locale' />
+    </NuxtLink>
+    <NuxtLink to="/login"> <ButtonsPrimary title='Torna indietro' /> </NuxtLink>
+  </div>
+</template>
+
+<style scoped>
+.myflex {
+  display: flex;
+  flex-flow: column nowrap;
+  justify-content: center;
+  align-items: center;
+  gap: 5px;
+  padding-top: 25px;
+}
+</style>
