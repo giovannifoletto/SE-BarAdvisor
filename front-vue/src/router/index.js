a1f431841a62352b5b9ab4d55754f6c6e717384f
diocarro <daniele.carraro@studenti.unitn.it>
2022-06-01 14:23:35 +0200

    router setup

--- front-vue/src/router/index.js
+++ front-vue/src/router/index.js
@@ -1,5 +1,7 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
+import LoginView from '../views/auth/LoginView'
+import RegistrazioneView from '../views/auth/RegistrazioneView'
 
 const routes = [
   {
@@ -8,12 +10,14 @@ const routes = [
     component: HomeView
   },
   {
-    path: '/about',
-    name: 'about',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/AboutView.vue')
+    path: '/login',
+    name: 'login',
+    component: LoginView
+  },
+  {
+    path: '/registrazione',
+    name: 'registrazione',
+    component: RegistrazioneView
   }
 ]
 
