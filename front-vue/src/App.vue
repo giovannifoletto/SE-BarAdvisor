a6b2561932695a1393f1b34695052695e7432bb2
Giovanni Foletto <giovanni.foletto@gmail.com>
2022-06-01 16:00:15 +0200

    Bootstrap style.

--- front-vue/src/App.vue
+++ front-vue/src/App.vue
@@ -1,13 +1,5 @@
 <template>
   <div>
-    <!-- CSS only -->
-    <link
-      href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/css/bootstrap.min.css"
-      rel="stylesheet"
-      integrity="sha384-0evHe/X+R7YkIZDRvuzKMRqM+OrBnVFBL6DOitfPri4tjfHxaWutUpFmBp4vmVor"
-      crossorigin="anonymous"
-    />
-
     <Header></Header>
     <div class="page">
       <RightDiv></RightDiv>
@@ -38,8 +30,8 @@ export default {
 </script>
 
 
-<style scoped>
-@import "@/assets/base.css";
+<style>
+@import "./assets/base.css";
 .page {
   display: grid;
   grid-template-columns: 25% auto 25%;
