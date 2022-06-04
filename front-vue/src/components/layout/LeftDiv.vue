edff1a2680f7072a6eedc5763a0da87660ccbc9d
Giovanni Foletto <giovanni.foletto@gmail.com>
2022-06-04 23:22:42 +0200

    Cambio .ateral section.

--- front-vue/src/components/layout/LeftDiv.vue
+++ front-vue/src/components/layout/LeftDiv.vue
@@ -1,15 +1,47 @@
-<script>
-export default {
-  name: 'Left Div',
-};
-</script>
-
 <template>
   <div>
-    <h1 v-if="$store.state.token">{{ $store.state.user.email }}</h1>
-    <h1 v-if="!$store.state.token"> Account </h1>
+    <h1 v-if="$store.state.token">Ciao, {{ username }}</h1>
+    <h1 v-if="!$store.state.token"> Esegui il Login per utilizzare al meglio la piattaforma. </h1>
   </div>
 </template>
+
+<script>
+import config from '@/config';
+export default {
+  name: 'Left Div',
+  data(){
+    return {
+      username: "",
+    }
+  },
+  async mounted(){
+    const opzioniRichiesta = {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+          "Authorization": `Bearer ${this.$store.state.token}`,
+        },
+        body: JSON.stringify(this.recensione),
+      };
+
+    try {
+      const fet = await fetch(`${baseURL}/auth/utenti/${this.$store.state.user.id}`, opzioniRichiesta )
+
+      const data = await fet.json();
+
+      if(data.success){
+        this.username = data.nomeUtente
+      } else {
+        this.username = ""
+      }
+
+
+    } catch (error) {
+      
+    }
+  }
+};
+</script>
 
 <style scoped>
 div {
