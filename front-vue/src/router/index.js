b7a47a5954bc3fda3c67cf0717ddd66c5da3625a
giovannifoletto <67295809+giovannifoletto@users.noreply.github.com>
2022-06-04 23:14:06 +0200

    Form di invio notifica creato. (#39)
    * Form di invio notifica creato.
    
    * Aggiunta v-if bottone invia notifica.

--- front-vue/src/router/index.js
+++ front-vue/src/router/index.js
@@ -19,6 +19,7 @@ import LocaleView from '@/views/locale/LocaleView.vue'
 import ProfiloPersonale from '@/views/account/ProfiloPersonale'
 
 import CreaEvento from '@/views/forms/CreaEvento.vue'
+import InviaNotifica from '@/views/forms/InviaNotifica.vue'
 import CambiaPassword from '@/views/forms/CambiaPassword.vue'
 
 import Error403 from '@/views/pagineErrore/403.vue'
@@ -71,6 +72,12 @@ const routes = [
     path: '/locali/:localeID',
     name: 'paginaLocale',
     component: LocaleView,
+    props: true
+  },
+  {
+    path: '/locali/:localeID/invianotifica/:eventoID',
+    name: 'formInviaNotifica',
+    component: InviaNotifica,
     props: true
   },
   {
