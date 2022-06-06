146fca56e8eb11c4ae33b40757fa996e40994320
diocarro <daniele.carraro@studenti.unitn.it>
2022-06-06 13:51:02 +0200

    preparazione a deploy

--- frontend/vue.config.js
+++ frontend/vue.config.js
@@ -1,4 +1,14 @@
 const { defineConfig } = require('@vue/cli-service')
-module.exports = defineConfig({
-  transpileDependencies: true
-})
+const path = require('path')
+
+module.exports = {
+  outputDir: path.resolve(__dirname, '../backend/public'),
+  devServer: {
+    proxy: {
+      '/api/v1': {
+        target: 'http://localhost:4000/api/v1'
+      }
+    }
+  },
+  transpileDependencies: true,
+}
