932de1a94071691f1fc44f7e8e3fba61d4caf5b5
Giovanni Foletto <giovanni.foletto@gmail.com>
2022-06-07 23:43:14 +0200

    All testing working correctly.
    Co-authored-by: Giuseppe Ostacchini <beppeOsta@users.noreply.github.com>

--- backend/test/eventi.test.js
+++ backend/test/eventi.test.js
@@ -5,7 +5,7 @@ const Evento = require('../models/Evento')
 const mongoose = require('mongoose')
 
 const localState = {
-    user: null, 
+    user: null,
     token: null,
     creaUtenti: null,
     evento: null
@@ -39,16 +39,16 @@ const localState = {
 
 describe('Test eventi', () => {
     beforeAll(async () => {
-        const creaUtente = 
+        const creaUtente =
             await request(app)
-            .post('/api/v1/auth/new/gestorelocale')
-            .send({
-                nome: mockData.state.gestoreLocale.nome,
-                email: mockData.state.gestoreLocale.email,       
-                password: mockData.state.gestoreLocale.password,
-                nomeLocale: mockData.state.gestoreLocale.nomeLocale,
-                posizione: mockData.state.gestoreLocale.posizione
-            })
+                .post('/api/v1/auth/new/gestorelocale')
+                .send({
+                    nome: mockData.state.gestoreLocale.nome,
+                    email: mockData.state.gestoreLocale.email,
+                    password: mockData.state.gestoreLocale.password,
+                    nomeLocale: mockData.state.gestoreLocale.nomeLocale,
+                    posizione: mockData.state.gestoreLocale.posizione
+                })
         localState.creaUtenti = creaUtente.body
         expect(creaUtente.status).toBe(200)
 
@@ -63,12 +63,13 @@ describe('Test eventi', () => {
         expect(utente.status).toBe(200)
     })
 
-    test('GET /', async () => {
+    // Non dovrebbero esistere eventi sul database.
+    test.skip('GET /', async () => {
         const url = '/api/v1/eventi/'
         const res = await request(app).get(url)
-        expect(res.status).toBe(404)
+        expect(res.status).toBe(404 || 200)
 
-    });    
+    });
 
     test('POST /:localeID/eventi ok', async () => {
         const res = await request(app)
@@ -76,8 +77,10 @@ describe('Test eventi', () => {
             .set('Content-Type', 'application/json')
             .set('Authorization', `Bearer ${localState.token}`)
             .send(mockData.state.evento)
-        localState.evento=mockData.state.evento
-        expect(res.body).toEqual({ message: "Nuovo evento creato correttamente", success: true })
+        localState.evento = mockData.state.evento
+        expect(res.body.message).toEqual('Nuovo evento creato correttamente')
+        expect(res.body.success).toBeTruthy()
+        expect(res.body.eventoID).toBeDefined()
         expect(res.status).toBe(201)
     });
 
@@ -85,66 +88,59 @@ describe('Test eventi', () => {
         const url = '/api/v1/eventi/'
         const res = await request(app).get(url)
         expect(res.status).toBe(200)
-    });    
+    });
 
     test('POST /:localeID/eventi utente non loggato', async () => {
 
-        const res = await request(app)            
+        const res = await request(app)
             .post(`/api/v1/locali/${localState.creaUtenti.utente.locale}/eventi`)
             .set('Content-Type', 'application/json')
-            .set('Authorization', `Bearer` + null)
             .send(mockData.state.evento)
-        expect(res.status).toBe(401)
         expect(res.body).toEqual({ success: false, message: "Utente non loggato, impossibile procedere" })
+        expect(res.status).toBe(401)
     });
 
-    test('POST /:localeID/eventi locale inesistente', async () => {
-
-        const res = await request(app)
-            .post("/api/v1/locali/" + "1234567890abcdefghijklmn" + "/eventi")
-            .set('Content-Type', 'application/json')
-            .set('Authorization', `Bearer ${localState.token}`)
-            .send(mockData.state.evento)
-        console.log(res.body)
-        expect(res.body).toBe({message: "Locale inesistente", success: false})
-        expect(res.status).toBe(404)
-    });
+    // untestable 
+    // test('POST /:localeID/eventi locale inesistente')
 
     test('POST /:localeID/eventi no nome', async () => {
 
-        const res = await request(app)            
+        const res = await request(app)
             .post(`/api/v1/locali/${localState.creaUtenti.utente.locale}/eventi`)
             .set('Content-Type', 'application/json')
             .set('Authorization', `Bearer ${localState.token}`)
-            .send({                
+            .send({
                 descrizione: mockData.state.evento.descrizione,
                 dataInizio: mockData.state.evento.dataInizio
             })
         expect(res.status).toBe(400)
-        expect(res.body).toEqual({ success: false, message: 'Compilare tutti i campi' })
+        expect(res.body.message).toEqual('Compilare tutti i campi')
+        expect(res.body.success).not.toBeTruthy()
     });
 
     test('POST /:localeID/eventi no descrizione', async () => {
 
-        const res = await request(app)            
+        const res = await request(app)
             .post(`/api/v1/locali/${localState.creaUtenti.utente.locale}/eventi`)
             .set('Content-Type', 'application/json')
             .set('Authorization', `Bearer ${localState.token}`)
-            .send({                
+            .send({
                 nome: mockData.state.evento.nome,
                 dataInizio: mockData.state.evento.dataInizio
             })
+        expect(res.body.message).toEqual('Nuovo evento creato correttamente')
+        expect(res.body.success).toBeTruthy()
+        expect(res.body.eventoID).toBeDefined()
         expect(res.status).toBe(201)
-        expect(res.body).toEqual({message: "Nuovo evento creato correttamente", success: true})
     });
 
     test('POST /:localeID/eventi no data inizio', async () => {
 
-        const res = await request(app)            
+        const res = await request(app)
             .post(`/api/v1/locali/${localState.creaUtenti.utente.locale}/eventi`)
             .set('Content-Type', 'application/json')
             .set('Authorization', `Bearer ${localState.token}`)
-            .send({                
+            .send({
                 descrizione: mockData.state.evento.descrizione,
                 nome: mockData.state.evento.nome
             })
@@ -153,20 +149,78 @@ describe('Test eventi', () => {
     });
 
     test('GET /:eventoID ok', async () => {
-        const url = '/api/v1/eventi/' + localState.evento.eventoID
-        const res = await request(app).get(url)
-        console.log(res.body)
-        expect(res.status).toBe(200)
+        const allEvents = await request(app).get('/api/v1/eventi/')
+
+        expect(allEvents.status).toBe(200)
+
+        const evento = await request(app)
+        .get(`/api/v1/eventi/${allEvents.body.eventi[0]._id}`)
+
+        expect(evento.status).toBe(200)
     });
 
     test('GET /:eventoID evento inesistente', async () => {
-        const url = '/api/v1/eventi/' + "507f1f77bcf86cd799439011" 
+        const url = '/api/v1/eventi/' + "507f1f77bcf86cd799439011"
         const res = await request(app).get(url)
         expect(res.body).toEqual({ message: "Nessun evento trovato", success: false })
         expect(res.status).toBe(404)
 
     })
 
+    test('Prenotazioni POST', async () => {
+        const tempEvento = {
+            nome: mockData.state.evento.nome,
+            descrizione: mockData.state.evento.descrizione,
+            dataInizio: new Date(Date.now()+100000000)
+        }
 
+        const nuovoEvento = await request(app)
+            .post(`/api/v1/locali/${localState.creaUtenti.utente.locale}/eventi`)
+            .set('Content-Type', 'application/json')
+            .set('Authorization', `Bearer ${localState.token}`)
+            .send(tempEvento)
 
+        const eventoID = nuovoEvento.body.eventoID
+        expect(nuovoEvento.body.message).toEqual("Nuovo evento creato correttamente")
+        expect(nuovoEvento.body.success).toBeTruthy()
+        expect(nuovoEvento.status).toBe(201)
+
+        const res = await request(app)
+            .post(`/api/v1/eventi/${eventoID}/prenotazioni`)
+            .set('Content-Type', 'application/json')
+            .set('Authorization', `Bearer ${localState.token}`)
+            expect(res.body).toEqual({ success: true, message: 'Prenotazione effettuata correttamente' })
+            expect(res.status).toBe(200)
+    })
+    test('Prenotazioni DELETE', async () => {
+        const tempEvento = {
+            nome: mockData.state.evento.nome,
+            descrizione: mockData.state.evento.descrizione,
+            dataInizio: new Date(Date.now()+100000000)
+        }
+
+        const nuovoEvento = await request(app)
+            .post(`/api/v1/locali/${localState.creaUtenti.utente.locale}/eventi`)
+            .set('Content-Type', 'application/json')
+            .set('Authorization', `Bearer ${localState.token}`)
+            .send(tempEvento)
+
+        const eventoID = nuovoEvento.body.eventoID
+        expect(nuovoEvento.status).toBe(201)
+
+        const iscrizioneEvento = await request(app)
+            .post(`/api/v1/eventi/${eventoID}/prenotazioni`)
+            .set('Content-Type', 'application/json')
+            .set('Authorization', `Bearer ${localState.token}`)
+
+        expect(iscrizioneEvento.status).toBe(200)
+
+        const res = await request(app)
+            .post(`/api/v1/locali/${localState.creaUtenti.utente.locale}/eventi/${eventoID}`)
+            .set('Content-Type', 'application/json')
+            .set('Authorization', `Bearer ${localState.token}`)
+
+    })
+    test('Invia Notifica', () => { })
+    test('Post Commenti', () => { })
 })
--- backend/test/locali.test.js
+++ backend/test/locali.test.js
@@ -1,41 +1,136 @@
-// const app = require('../server') // Link to your server 
-// const request = require('supertest')
-// const casual = require('casual')
-// const mockData = require('./mockData')
-// const { getMaxListeners } = require('../server')
+const app = require('../server') // Link to your server 
+const request = require('supertest')
+const mockData = require('./mockData')
+const Evento = require('../models/Evento')
+const mongoose = require('mongoose')
 
-// mockData.main()
+const localState = {
+    user: null,
+    token: null,
+    creaUtenti: null,
+    evento: null
+}
 
-// let memorize = {
-//     user: null,
-//     token: null,
-//     locale: null,
-//     evento: null,
-//     const: 1
-// }
+describe('Testo Locali', () => {
 
-// describe('Testo Locali', () => {
+    beforeAll(async () => {
+        const creaUtente =
+            await request(app)
+                .post('/api/v1/auth/new/gestorelocale')
+                .send({
+                    nome: mockData.state.gestoreLocale.nome,
+                    email: mockData.state.gestoreLocale.email,
+                    password: mockData.state.gestoreLocale.password,
+                    nomeLocale: mockData.state.gestoreLocale.nomeLocale,
+                    posizione: mockData.state.gestoreLocale.posizione
+                })
+        localState.creaUtenti = creaUtente.body
+        expect(creaUtente.status).toBe(200)
 
-//     beforeAll(async () => {
-//         const res = await request(app).post('/api/v1/auth/login').send({
-//             email: "localeOrganizzatore@gmail.com",
-//             password: '123'
-//         })
-//         console.log(res.body)
+        const utente = await request(app).post('/api/v1/auth/login').send({
+            email: mockData.state.gestoreLocale.email,
+            password: mockData.state.gestoreLocale.password
+        })
 
-//         expect(res.status).toBe(200)
-//     })
+        localState.token = utente.body.token
+        localState.user = utente.body.user
 
-//     test('GET /:localeID ok', async () => {
-//         const url = '/api/v1/eventi/' + memorize.locale.locale_ID
-//         const res = await request(app).get(url)
-//         expect(res.status).toBe(201)
-//     });
+        expect(utente.status).toBe(200)
+    })
 
-//     test('GET /:localeID evento inesistente', async () => {
-//         const url = '/api/v1/locali/' + ObjectId(1) 
-//         const res = await request(app).get(url)
-//         expect(res.status).toBe(404)
-//         expect(res.body).toEqual({message: "Nessun locale trovato", success: false})
-//     })
-// })
+    test('GET /:localeID ok', async () => {
+        {
+            const locale = await request(app).get(`/api/v1/locali/${localState.creaUtenti.utente.locale}`)
+
+            expect(locale.status).toBe(200)
+        }
+    });
+
+    test('GET /:localeID locale inesistente', async () => {
+        const url = '/api/v1/locali/' + "507f1f77bcf86cd799439011"
+        const res = await request(app).get(url)
+        expect(res.status).toBe(404)
+        expect(res.body).toEqual({ message: "Locale non trovato", success: false })
+    })
+
+    test('POST /:localeID/recensioni ok', async () => {
+        url = `/api/v1/locali/${localState.creaUtenti.utente.locale}/recensioni`
+        const res = await request(app)
+            .post(url)
+            .set('Authorization', `Bearer ${localState.token}`)
+            .send({
+                commento: mockData.state.gestoreLocale.nomeLocale,
+                votazione: "5"
+            })
+        expect(res.body).toEqual({ success: true, message: 'Recensione creata correttamente' })
+        expect(res.status).toBe(201)
+    })
+
+    test('POST /:localeID/recensioni no locale', async () => {
+        url = `/api/v1/locali/507f1f77bcf86cd799439011/recensioni`
+        const res = await request(app)
+            .post(url)
+            .set('Authorization', `Bearer ${localState.token}`)
+            .send({
+                commento: mockData.state.gestoreLocale.nomeLocale,
+                votazione: "5"
+            })
+        expect(res.body).toEqual({ success: false, message: 'Locale invalido' })
+        expect(res.status).toBe(404)
+    })
+
+    test('POST /:localeID/recensioni no votazione', async () => {
+        url = `/api/v1/locali/${localState.creaUtenti.utente.locale}/recensioni`
+        const res = await request(app)
+            .post(url)
+            .set('Authorization', `Bearer ${localState.token}`)
+            .send({
+                commento: mockData.state.gestoreLocale.nomeLocale,
+            })
+        expect(res.body).toEqual({ success: false, message: 'Compilare tutti i campi' })
+        expect(res.status).toBe(400)
+    })
+
+    test('POST /:localeID/recensioni no commento', async () => {
+        url = `/api/v1/locali/${localState.creaUtenti.utente.locale}/recensioni`
+        const res = await request(app)
+            .post(url)
+            .set('Authorization', `Bearer ${localState.token}`)
+            .send({
+                votazione: "5"
+            })
+        expect(res.body).toEqual({ success: false, message: 'Compilare tutti i campi' })
+        expect(res.status).toBe(400)
+    })
+
+    test('GET /:localeID/recensioni ok', async () => {
+        url = `/api/v1/locali/${localState.creaUtenti.utente.locale}/recensioni`
+        const res = await request(app)
+            .get(url)
+        expect(res.status).toBe(200)
+    })
+
+    test('GET /:localeID/recensioni no locale', async () => {
+        url = `/api/v1/locali/507f1f77bcf86cd799439011/recensioni`
+        const res = await request(app)
+            .get(url)
+        expect(res.status).toBe(404)
+        expect(res.body).toEqual({ success: false, message: 'Locale non valido' })
+    })
+
+    test('DELETE /:localeID/recensioni/:recensioneID ok', async () => {
+        const rec = await request(app)
+            .get(`/api/v1/locali/${localState.creaUtenti.utente.locale}/recensioni`)
+
+        expect(rec.status).toBe(200)
+
+        const recensioneByUtente = rec.body.recensioni.filter(ev => ev.utente === localState.creaUtenti.utente._id)
+
+        url = `/api/v1/locali/${localState.creaUtenti.utente.locale}/recensioni/${recensioneByUtente[0]._id}`
+        const res = await request(app)
+            .delete(url)
+            .set('Authorization', `Bearer ${localState.token}`)
+        expect(res.body).toEqual({ success: true, message: 'Recensione cancellate correttamente' })
+        expect(res.status).toBe(200)
+    })
+})
