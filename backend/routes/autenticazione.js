3725e374cde0308db37430759f8c4c490157a93a
Giuseppe <beppe.osta@gmail.com>
2022-06-09 22:42:42 +0200

    Cancellata documentazione
    Spostata su apiary

--- backend/routes/autenticazione.js
+++ backend/routes/autenticazione.js
@@ -5,212 +5,23 @@ const controllerUtenti = require('../controllers/controllerUtente')
 
 const checkAuth = require('../middleware/check-auth')
 
-/**
- * Ottenere tutti gli utenti nel database (per development)
- * @openapi
- * /auth/utenti:
- *  get:
- *    description: Ritorna tutti gli utenti per developmente oppure per la pagina di admin.
- *    response:
- *      200: 
- *        description: Ritorna tutti gli utenti.
- *        content:
- *          application/json:
- *            schema:
- *              type: object
- *              properties:
- *                success:
- *                  type: boolean
- *                utenti:
- *                  type: array
- *                  description: array di tutti gli utenti presenti sul database.
- *      500:
- *        description: Ritorna un errore.
- *        content: 
- *          application/json:
- *            schema:
- *              type: object
- *              properties:
- *                success: 
- *                  type: boolean
- *                error:
- *                  type: string
- *                          
- */
+
 router.get('/utenti', controllerUtenti.getAllUtenti)
 
-/**
- * Ottenere il nome utente di un utente
- * @openapi
- * /auth/utenti/:utenteID:
- *  get:
- *    description: Ritorna il nome utente di un utente
- *    response:
- *      200: 
- *        description: Ritorna tutti gli utenti.
- *        content:
- *          application/json:
- *            schema:
- *              type: object
- *              properties:
- *                success:
- *                  type: boolean
- *                utenti:
- *                  type: array
- *                  description: array di tutti gli utenti presenti sul database.
- *      
- */
 router.get('/utenti/:utenteID', controllerUtenti.getNomeUtente)
 
-/**
- * creazione di Gestore Locale e relativo Locale
- * @openapi
- * /auth/new/gestorelocale:
- *  post:
- *    description: Crea un Gestore Locale
- *    response:
- *      201: 
- *        description: Ritorna l'id del Locale e del suo Gestore Locale
- *        content:
- *          application/json:
- *            schema:
- *              type: object
- *              properties:
- *                success:
- *                  type: boolean
- *                locale:
- *                  type: Locale
- *                  description: Schema Locale
- *               utente: 
- *                 type: Utente
- */
 router.post('/new/gestorelocale', controllerUtenti.registrazioneLocale)
 
-/**
- * Creazione di Cliente
- * @openapi
- * /auth/new/cliente:
- *  post:
- *    description: Crea un Cliente
- *    response:
- *      201: 
- *        description: Ritorna l'id dell'utente
- *        content:
- *          application/json:
- *            schema:
- *              type: object
- *              properties:
- *                success:
- *                  type: boolean
- *                utente: 
- *                 type: Utente
- */
 router.post('/new/cliente', controllerUtenti.registrazioneCliente)
 
-/**
- * login Utente
- * @openapi
- * /auth/login:
- *  post:
- *    description: Permette di effettuare il login
- *    response:
- *      200: 
- *        description: Ritorna l'id dell'utente
- *        content:
- *          application/json:
- *            schema:
- *              type: object
- *              properties:
- *                success:
- *                  type: boolean
- *               token: 
- *                 type: jwt
- */
 router.post('/login', controllerUtenti.loginUtente)
 
-
-/**
- * Password dimenticata
- * @openapi
- * /auth/passworddimenticata:
- *  post:
- *    description: Permette di recuperare la password
- *    response:
- *      200: 
- *        description: Ritorna un messaggio di conferma dell'invio della mail di recupero
- *        content:
- *          application/json:
- *            schema:
- *              type: object
- *              properties:
- *                success:
- *                  type: boolean
- *               messaggio: 
- *                 type: String
- */
 router.post('/passworddimenticata', controllerUtenti.passwordDimenticata)
 
-/** 
- * Reset password - continua passoword dimenticata
- * @openapi
- * /auth/resetpassword/:resetToken:
- *  put:
- *    description: Permette di generare un token per modificare la password
- *    response:
- *      200: 
- *        description: Ritorna un messaggio di conferma cambio password
- *        content:
- *          application/json:
- *            schema:
- *              type: object
- *              properties:
- *                success:
- *                  type: boolean
- *                message: 
- *                  type: String
- */
 router.put('/resetpassword/:resetToken', controllerUtenti.resetToken)
 
-/**
- * Cambio password - diverso da password dimenticata
- * @openapi
- * /auth/cambiopassword:
- *  put:
- *    description: Permette di cambiare la password
- *    response:
- *      200: 
- *        description: Ritorna un messaggio di conferma cambio password
- *        content:
- *          application/json:
- *            schema:
- *              type: object
- *              properties:
- *                success:
- *                  type: boolean
- *               message: 
- *                 type: String
- */
 router.put('/cambiopassword', checkAuth, controllerUtenti.changePassword)
 
-/**
- * Cancellazione utente Cliente
- * @openapi
- * /auth/cambiopassword:
- *  delete:
- *    description: Permette di eliminare il proprio profilo utente
- *    response:
- *      200: 
- *        description: Ritorna un messaggio di conferma cancellazione account
- *        content:
- *          application/json:
- *            schema:
- *              type: object
- *              properties:
- *                success:
- *                  type: boolean
- *               message: 
- *                  type: String
- */
 router.delete('/utenti/:utenteID', checkAuth, controllerUtenti.deleteAccount)
 
 module.exports = router
--- backend/routes/locali.js
+++ backend/routes/locali.js
@@ -8,287 +8,41 @@ const controllerEventi = require('../controllers/controllerEvento')
 const controllerLocali = require('../controllers/controllerLocale')
 const controllerRecensioni = require('../controllers/controllerRecensione')
 
-/* @openapi
-* /locali/:localeID
-*  get:
-*    description: Permette di visualizzare un locale specifico
-*    response:
-*      200: 
-*        description: Ritorna le informazioni di un locale
-*        content:
-*          application/json:
-*            schema:
-*              type: object
-*              properties:
-*                success:
-*                  type: boolean
-                 locale: 
-                   type: Locale
-                 eventiPassati:
-                   type: array
-                   description: array di eventi già avvenuti
-                 eventiFuturi
-                   type: array
-                   description: array di eventi che devono ancora avvenire
-*/
-
 // recuperare un locale 
 router.get('/:localeID', controllerLocali.getLocale)
-
-/* @openapi
-* /locali/
-*  get:
-*    description: Permette di visualizzare tutti i locali che devono ancora essere verificati
-*    response:
-*      200: 
-*        description: Ritorna l'elenco dei locali da verificare
-*        content:
-*          application/json:
-*            schema:
-*              type: object
-*              properties:
-*                success:
-*                  type: boolean
-                 locali: 
-                   type: Array
-                   description: Array di locali che devono ancora essere verificati
-                 
-*/
 
 // recuperare i locali da verificare (per admin)
 router.get('/', checkAuth, checkRole.checkPermessiAdmin, controllerLocali.getLocaliDaConfermare)
 
-/* @openapi
-* /locali/
-*  post:
-*    description: Permette di effettuare la verifica di un locale
-*    response:
-*      200: 
-*        description: Ritorna un messaggio di conferma avvenuta verifica
-*        content:
-*          application/json:
-*            schema:
-*              type: object
-*              properties:
-*                success:
-*                  type: boolean
-                 message:
-                   type: String
-                 
-*/
-
 // verificare un locale (admin)
 router.post('/:localeID/verifica', checkAuth, checkRole.checkPermessiAdmin, controllerLocali.verificaLocale)
-
-/* @openapi
-* /locali/:localeID/eventi
-*  post:
-*    description: Permette al Gestore Locale di creare un evento
-*    response:
-*      201: 
-*        description: Ritorna un messaggio di avvenuta creazione evento e l'id dell'evento
-*        content:
-*          application/json:
-*            schema:
-*              type: object
-*              properties:
-*                success:
-*                  type: boolean
-                 message: 
-                   type: String
-                 eventoID:
-                   type: String
-*/
 
 // aggiungere un evento 
 router.post('/:localeID/eventi', checkAuth, checkRole.checkPermessiProprietarioLocale, controllerEventi.postEvento)
 
-/* @openapi
-* /locali/:localeID/eventi/:eventoID/
-*  get:
-*    description: Permette di visualizzare le informazioni di un evento specifico
-*    response:
-*      200: 
-*        description: Ritorna i dati di un evento
-*        content:
-*          application/json:
-*            schema:
-*              type: object
-*              properties:
-*                success:
-*                  type: boolean
-                 evento: 
-                   type: Evento
-*/
-
 // informazioni relative ad un evento specifico
 router.get('/:localeID/eventi/:eventoID', controllerEventi.getEvento)
-
-/* @openapi
-* /locali/:localeID/eventi/:eventoID
-*  put:
-*    description: Permette al Gestore Locale di modificare i dati di un evento specifio
-*    response:
-*      200: 
-*        description: Ritorna un messaggio di conferma aggiornamento dati
-*        content:
-*          application/json:
-*            schema:
-*              type: object
-*              properties:
-*                success:
-*                  type: boolean
-                 message: 
-                   type: String
-*/
 
 // modifica le informazioni di un evento specifico
 router.put('/:localeID/eventi/:eventoID', checkAuth, checkRole.checkPermessiProprietarioLocale, controllerEventi.modificaEvento)
 
-/* @openapi
-* /locali/:localeID/eventi/:eventoID
-*  delete:
-*    description: Permette al Gestore Locale di cancellare un evento specifico
-*    response:
-*      200: 
-*        description: Ritorna un messaggio di conferma cancellazione evento
-*        content:
-*          application/json:
-*            schema:
-*              type: object
-*              properties:
-*                success:
-*                  type: boolean
-                 message: 
-                   type: String
-*/
-
 // eliminare un evento
 router.delete('/:localeID/eventi/:eventoID', checkAuth, checkRole.checkPermessiProprietarioLocale, controllerEventi.deleteEvento)
-
-/* @openapi
-* /locali/:localeID/recensioni
-*  get:
-*    description: Permette di visualizzare tutte le recensioni di un locale
-*    response:
-*      200: 
-*        description: Ritorna l'elenco di tutte le recensioni del locale
-*        content:
-*          application/json:
-*            schema:
-*              type: object
-*              properties:
-*                success:
-*                  type: boolean
-                 recensioni: 
-                   type: array
-                   description: Array di tutte le recensioni di un locale
-*/
 
 // restituisce tutte le recensioni di un locale
 router.get('/:localeID/recensioni', controllerRecensioni.getAllRecensioni)
 
-/* @openapi
-* /:localeID/recensioni
-*  post:
-*    description: Permette a un utente di creare un commento
-*    response:
-*      201: 
-*        description: Ritorna un messaggio di conferma creazione commento
-*        content:
-*          application/json:
-*            schema:
-*              type: object
-*              properties:
-*                success:
-*                  type: boolean
-                 message: 
-                   type: String
-*/
-
 // creazione di una recensione
 router.post('/:localeID/recensioni', checkAuth, controllerRecensioni.postRecensione)
-
-/* @openapi
-* /:localeID/segui
-*  delete:
-*    description: Permette a un utente di seguire un locale
-*    response:
-*      200: 
-*        description: Ritorna un messaggio di conferma che si è iniziato a seguire il locale
-*        content:
-*          application/json:
-*            schema:
-*              type: object
-*              properties:
-*                success:
-*                  type: boolean
-                 message: 
-                   type: String
-*/
 
 // seguire un locale
 router.post('/:localeID/segui', checkAuth, controllerLocali.followLocale)
 
-/* @openapi
-* /:localeID/segui
-*  delete:
-*    description: Permette a un utente di smettere di seguire un locale
-*    response:
-*      200: 
-*        description: Ritorna un messaggio di conferma che ha smesso di seguire il locale
-*        content:
-*          application/json:
-*            schema:
-*              type: object
-*              properties:
-*                success:
-*                  type: boolean
-                 message: 
-                   type: String
-*/
-
 // non seguire più un locale
 router.delete('/:localeID/segui', checkAuth, controllerLocali.unfollowLocale)
 
-/* @openapi
-* /locali/:localeID/recensioni/:recensioneID
-*  delete:
-*    description: Permette a un Admin o all'utente che la ha scritta di cancellare una recensione
-*    response:
-*      200: 
-*        description: Ritorna un messaggio di conferma cancellazione recensione
-*        content:
-*          application/json:
-*            schema:
-*              type: object
-*              properties:
-*                success:
-*                  type: boolean
-                 message: 
-                   type: String
-*/
-
 // cancella una recensione
 router.delete('/:localeID/recensioni/:recensioneID', checkAuth, checkRole.checkPermessiCreatoreRecensione, controllerRecensioni.deleteRecensione)
-
-/* @openapi
-* /:localeID/eventi/:eventoID/notifiche
-*  post:
-*    description: Permette al Gestore Locale di mandare una notifica a tutti gli iscritti a un evento
-*    response:
-*      200: 
-*        description: Ritorna un messaggio di conferma invio segnalazione
-*        content:
-*          application/json:
-*            schema:
-*              type: object
-*              properties:
-*                success:
-*                  type: boolean
-                 message: 
-                   type: String
-*/
 
 // invio notifica ai partecipanti di un determinato evento
 router.post('/:localeID/eventi/:eventoID/notifiche', checkAuth, checkRole.checkPermessiProprietarioLocale, controllerEventi.invioNotifica)
